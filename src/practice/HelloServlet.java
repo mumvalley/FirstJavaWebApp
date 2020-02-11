5615e9b2379c523851cee54c938ddd87e92478c3
Masaru Kikutani <mum0723@gmail.com>
2020-02-11 16:21:57 +0900

    JavaAppのディレクトリをコミット

---
+++ src/practice/HelloServlet.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.io.*;
+import javax.servlet.*;
+
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public HelloServlet() {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
