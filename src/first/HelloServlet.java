1a0cde2dc72e070a8e140d2b42ee913048b58110
Masaru Kikutani <mum0723@gmail.com>
2020-02-12 06:42:32 +0900

    占いページ完成

--- src/first/HelloServlet.java
+++ src/first/HelloServlet.java
@@ -1,32 +1,57 @@
 package first;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloServlet
- */
-@WebServlet("/HelloServlet")
+
+@WebServlet("/SampleServlet")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	protected void doGet(
+			HttpServletRequest request,
+			HttpServletResponse response
+			) throws ServletException, IOException {
+		// 運勢をランダムに決定
+		String[] luckArray = {"超すっきり", "すっきり", "最悪"};
+		int index = (int)(Math.random() * 3);
+		String luck = luckArray[index];
+		
+		// 実行日を取得
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM月dd日");
+		String today = sdf.format(date);
+		
+		// htmlを出力
+//		response.setContentType("text/html; charset=UTF-8");
+		response.setContentType("ABCDE");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>すっきり占い</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>" + today + "の運勢は「" + luck + "」です</p>");
+		out.println("</body>");
+		out.println("</html>");
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+	protected void doPost(
+			HttpServletRequest request,
+			HttpServletResponse response
+			) throws ServletException, IOException {
+		
 		doGet(request, response);
 	}
 
