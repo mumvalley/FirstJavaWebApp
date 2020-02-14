cca24b80ccc1ec3288bd73462d407d797a3e7ff5
Masaru Kikutani <mum0723@gmail.com>
2020-02-14 09:24:27 +0900

    リクエストパラメータ送信

--- src/first/HelloServlet.java
+++ src/first/HelloServlet.java
@@ -32,8 +32,7 @@ public class HelloServlet extends HttpServlet {
 		String today = sdf.format(date);
 		
 		// htmlを出力
-//		response.setContentType("text/html; charset=UTF-8");
-		response.setContentType("ABCDE");
+		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<head>");
